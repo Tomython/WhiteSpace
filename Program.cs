7420f3984765ca8a96686ae5a630b41aae076994
Tomython <dmitrymacpro@gmail.com>
2025-02-05 22:38:01 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace MinimalWebServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls("http://*:5000");
+                    webBuilder.Configure(app =>
+                    {
+                        app.UseDefaultFiles();
+                        app.UseStaticFiles(); // Эта строка для обслуживания статичных файлов (например, index.html)
+                    });
+                });
+    }
+}
